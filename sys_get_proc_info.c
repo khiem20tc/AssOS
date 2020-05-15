c27cd1eb45f77d74f359820af04afe39630d6fdc
Nguyen Huynh Huu Khiem <53548789+khiem20tc@users.noreply.github.com>
2020-05-15 01:20:53 -0700

    Add files via upload

---
+++ sys_get_proc_info.c
@@ -0,0 +1,54 @@
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+
+struct proc_info {
+	pid_t pid;
+	char name[16];
+};
+struct procinfos {
+	long studentID;
+	struct proc_info proc;
+	struct proc_info parent_proc;
+	struct proc_info oldest_child_proc;
+};
+//asmlinkage long sys_get_proc_info(pid_t pid, struct procinfos *info)
+SYSCALL_DEFINE2(get_proc_info, pid_t, pid, struct procinfos *, info)
+{
+	printk("Student ID: 1810998");
+	info->studentID = 1810998;
+	if (pid == -1) {
+		pid = current->pid;
+	}
+	struct task_struct *proces;
+	for_each_process (proces) {
+		if (proces->pid == pid) {
+			struct task_struct *cProces;
+			cProces = list_first_entry_or_null(
+				&proces->children, struct task_struct, sibling);
+			info->proc.pid = proces->pid;
+			strcpy(info->proc.name, proces->comm);
+			if (proces->real_parent == NULL) {
+				info->parent_proc.pid = 0;
+				//info->parent_proc.name = "\0";
+				strcpy(info->parent_proc.name, "\0");
+			} else {
+				info->parent_proc.pid =
+					proces->real_parent->pid;
+				//info->patent_proc.name = proces->real_parent->comm;
+				strcpy(info->parent_proc.name,
+				       proces->real_parent->comm);
+			}
+			if (cProces == NULL) {
+				info->oldest_child_proc.pid = 0;
+				//info->oldest_child_proc.name = "\0";
+				strcpy(info->oldest_child_proc.name, "\0");
+			} else {
+				info->oldest_child_proc.pid = cProces->pid;
+				//info->oldest_child_proc.name = cProces->comm;
+				strcpy(info->oldest_child_proc.name,
+				       cProces->comm);
+			}
+		}
+	}
+	return EINVAL;
+}
